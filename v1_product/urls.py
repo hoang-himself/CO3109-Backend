fb7c981dd0b40822e8d9af04b372b9fe862e50db
Hoang NGUYEN <baauco@gmail.com>
2022-04-14 15:58:33 +0000

    🚚 Rename url names

--- v1_product/urls.py
+++ v1_product/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'v1_product'
 
 urlpatterns = [
-    path('all', views.get_all, name='product_all'),
-    # path('popular', views.get_popular, name='product_popular'),
-    # path('related', views.get_related, name='product_related'),
-    path('filter', views.search_by_filter, name='product_filter'),
-    path('search', views.search_by_name, name='product_search'),
+    path('all', views.get_all, name='all'),
+    path('filter', views.search_by_filter, name='filter'),
+    path('search', views.search_by_name, name='search'),
 ]
