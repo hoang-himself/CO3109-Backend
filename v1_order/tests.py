286ecff49cbfb60f10a8b84741edc80a3b3a3d53
Hoang NGUYEN <baauco@gmail.com>
2022-04-20 09:03:34 +0700

    ✅ Add test to get orders with account

--- v1_order/tests.py
+++ v1_order/tests.py
@@ -42,6 +42,12 @@ class OrderTests(APITestCase):
         self.assertIsNotNone(token)
         self.header = {'HTTP_AUTHORIZATION': f'JWT {token}'}
 
+    def test_get_orders(self):
+        url = reverse('v1_order:self_orders')
+        client = APIClient()
+        response = client.get(url, **self.header)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_view_order(self):
         url = reverse('v1_order:view') + '?order_id=69'
         client = APIClient()
