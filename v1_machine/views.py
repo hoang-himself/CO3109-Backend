fd3b39b534d9b9c9ce9ca8e8dd87cd4391779492
Hoang NGUYEN <baauco@gmail.com>
2022-04-23 23:27:25 +0700

    🚀 Deploy /api/v1/machine/clear_order

--- v1_machine/views.py
+++ v1_machine/views.py
@@ -8,27 +8,6 @@ from mainframe.models import (Order, Product, Machine)
 from mainframe.serializers import (EnhancedModelSerializer, MachineSerializer)
 
 
-class InternalMachineSerializer(EnhancedModelSerializer):
-    class Meta:
-        model = Machine
-        fields = '__all__'
-
-
-class ProductField(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('uuid', 'image', 'name', 'price')
-
-
-class ImplicitOrder(EnhancedModelSerializer):
-    machine_uuid = serializers.CharField(source='machine.uuid')
-    item = ProductField()
-
-    class Meta:
-        model = Order
-        fields = ('machine_uuid', 'order_uuid', 'item', 'quantity')
-
-
 def request_header_to_machine(request):
     uuid_header = request.headers.get('X-MACHINE-UUID', None)
     if (uuid_header is None):
@@ -51,6 +30,27 @@ def about_self(request):
     )
 
 
+class InternalMachineSerializer(EnhancedModelSerializer):
+    class Meta:
+        model = Machine
+        fields = '__all__'
+
+
+class ProductField(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('uuid', 'image', 'name', 'price')
+
+
+class ImplicitOrderSerializer(EnhancedModelSerializer):
+    machine_uuid = serializers.CharField(source='machine.uuid')
+    item = ProductField()
+
+    class Meta:
+        model = Order
+        fields = ('machine_uuid', 'order_uuid', 'item', 'quantity')
+
+
 @api_view(['GET'])
 @permission_classes([permissions.AllowAny])
 def get_order_queue(request):
@@ -58,17 +58,23 @@ def get_order_queue(request):
     machine_data = InternalMachineSerializer(machine_obj).data
     return Response(
         status=status.HTTP_200_OK,
-        data=ImplicitOrder(
-            Order.objects.filter(machine=machine_data.get('id')
-                                ).order_by('order_uuid'),
+        data=ImplicitOrderSerializer(
+            Order.objects.filter(machine=machine_data.get('id'),
+                                 is_paid=False).order_by('order_uuid'),
             many=True
         ).data
     )
 
 
-@api_view(['DELETE'])
+@api_view(['POST'])
 @permission_classes([permissions.AllowAny])
 def clear_order(request):
-    # TODO After machine successfully dispenses drinks, clear order from table
-    # and add to prod_hist
+    if (order_uuid := request.data.get('order_uuid', None)) is None:
+        raise exceptions.ParseError({'order_uuid': 'This field is required'})
+
+    order_queryset = Order.objects.filter(order_uuid=order_uuid, is_paid=False)
+    if not order_queryset.exists():
+        raise exceptions.NotFound({'order_uuid': 'Order not found'})
+
+    # We can add an is_dispensed field here, but I'm lazy
     return Response(status=status.HTTP_200_OK, data=['Ok'])
