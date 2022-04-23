cf75bdcb9b467183a5ccbdfe1813a757a29ca97c
Hoang NGUYEN <baauco@gmail.com>
2022-04-23 04:06:03 +0000

    ♻️ Rename function

--- v1_order/tests.py
+++ v1_order/tests.py
@@ -99,7 +99,7 @@ class OrderTests(APITestCase):
         response = client.delete(url, data, **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_set_machine(self):
+    def test_checkout(self):
         url = reverse('v1_order:checkout')
         client = APIClient()
         data = {
